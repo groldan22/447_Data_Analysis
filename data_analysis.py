b290af39d08dbf2fea728c07428f37d7015623e5
Gerson <80487990+groldan22@users.noreply.github.com>
2022-10-18 16:40:14 -0400

    new comments, changed variables, new folder
    added comments for clarity. Edited all the "--- end of ___---" comments and opted for just "--------" and the end of each section for clarity as well. I added a folder to store out PNG exports.

--- data_analysis.py
+++ data_analysis.py
@@ -4,60 +4,65 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tkr
 
+# ---- set universal paths here ----
 # read current working directory
-print(os.getcwd())
-# create base univeral path for importing of CSVs. add CSVs below as needed using
-# the main path variable created below
+# print(os.getcwd())
+# create univeral path for current working directory.
 path = Path(__file__).parent.absolute()
 path = str(path)
 
-# store data path here
-data_path = str(path) + "/data"
+# path to data, cleanData, and pngs folder here. 
+data_path = str(path) + "/data/"
+cleanData_folder = str(path) + "/cleanData/"
+pngs_folder = str(path) + "/pngs/"
 
-# store cleanData path here
-clean_data_path = str(path) + "/cleanData/"
+# set file paths to INGEST here.
+state_stats_path = data_path + "State_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"
 
-# set path names here
-state_stats_path = data_path + "/State_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"
+# --------------------
 
 # ----- create data frames here -----
 df = pd.read_csv(state_stats_path)
 
-# ---- end of data frame creation ---
+# --------------------
 
 
-# ---- country wide analysis ----
+# ---- country wide manipulation ----
 # !!!reformat so that all other states have their own data frame in similar format is needed!!!
 # !!!print data frame df to see old vs "clean". They should look the same!!!
 region_list = ["Maryland", "Virginia", "District of Columbia"]
 # remove all states but Maryland Virginia and DC
-df_regions = df[df.RegionName.isin(region_list) == True]
+df_state = df[df.RegionName.isin(region_list) == True]
 #remove unecessary columns
-df_regions = df_regions.drop(["RegionID"],1)
-df_regions = df_regions.drop(["SizeRank"],1)
+df_state = df_state.drop(["RegionID"],1)
+df_state = df_state.drop(["SizeRank"],1)
 
 # transpose + clean 
-df_regions = df_regions.T
-df_regions.reset_index(inplace=True, drop=False)
+df_state = df_state.T
+df_state.reset_index(inplace=True, drop=False)
 # update header to first row then delete un-needed rows. Make print statements between rows to see changes.
-df_regions = df_regions.rename(columns=df_regions.iloc[0])
-df_regions.drop(df_regions.index[0], inplace = True)
-df_regions.drop(df_regions.index[0], inplace = True)
-df_regions.drop(df_regions.index[0], inplace = True)
-df_regions.rename(columns = {'RegionName':'Date'}, inplace = True)
-df_regions.reset_index(inplace=True, drop=True)
+df_state = df_state.rename(columns=df_state.iloc[0])
+df_state.drop(df_state.index[0], inplace = True)
+df_state.drop(df_state.index[0], inplace = True)
+df_state.drop(df_state.index[0], inplace = True)
+df_state.rename(columns = {'RegionName':'Date'}, inplace = True)
+df_state.reset_index(inplace=True, drop=True)
 
-# plot 
+# --------------------
 
-
-
+# ---- country wide plot and csv export ----
+# modify plot and show
 plt.xlabel("Year")
 plt.ylabel("Price")
-plt.plot(df_regions["Maryland"], label = "MD")
-plt.plot(df_regions["Virginia"], label = "VA")
-plt.plot(df_regions["District of Columbia"], label = "DC")
+plt.plot(df_state["Maryland"], label = "MD")
+plt.plot(df_state["Virginia"], label = "VA")
+plt.plot(df_state["District of Columbia"], label = "DC")
 plt.legend()
+plt.savefig(pngs_folder + 'state_time_series.png')
 plt.show()
 
 # export table
-df_regions.to_csv(clean_data_path + " regions_table.csv")
+df_state.to_csv(cleanData_folder + "regions_table.csv")
+
+# --------------------
+
