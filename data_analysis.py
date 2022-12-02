8cd8b784f16fa40d339839322533504641bf6d3d
ViphuKN <viphu12@gmail.com>
2022-12-01 22:00:29 -0500

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -170,7 +170,7 @@ maryland3 = maryland2.groupby(
     ['CountyName', 'Date', 'Value']).size().sort_values(ascending=False)
 
 # Export to CSV
-# maryland3.to_csv(data_path + 'maryland_raw_county_data.csv')
+maryland3.to_csv(data_path + 'maryland_raw_county_data.csv')
 
 # Ingest the file
 maryland4 = data_path + 'maryland_raw_county_data.csv'
